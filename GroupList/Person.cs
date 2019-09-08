862174b673f2c4d69577b6ff622a20a89bc1ce26
Olga Trikk <prymnesium13@gmail.com>
2019-09-08 15:36:37 +0300

    Add project files.

---
+++ GroupList/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GroupList
+{
+	public class Person
+	{
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public Person(string firstName, string lastName)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+		}
+	}
+}
---
+++ GroupList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GroupList
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] lines = File.ReadAllLines(@"C:\Users\prymn\Documents\list.txt");
+			foreach(var line in lines)
+			{
+				Console.WriteLine(line);
+			}
+
+			List<Person> students = new List<Person>();
+			foreach(var line in lines)
+			{
+				var names = line.Split(' ');
+				var person = new Person(names[0], names[1]);
+
+				students.Add(person);
+			}
+
+			foreach (var student in students)
+			{
+				Console.WriteLine(student.LastName + ", " + student.FirstName);
+			}
+		}
+	}
+}
